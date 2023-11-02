1e814c02bc2060a57f0e9a56b8c61ddab51caae9
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-11-02 09:28:07 +0100

    Create donutplot.py

---
+++ donutplot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+### primary colors
+## Orange, warm black
+primary_colors = ["#f26649", "#302026"]
+
+### secondary colors
+## Dk. grey, red, yellow, blue, aqua, lt. yellow
+secondary_colors = ["#525350", "#f15f5b", "#fce164", "#1c7ca1", "#71c3b4", "#fcf7ba"]
+
+title_font = {"fontname": "Century Gothic"}
+text_font = {"fontname": "Arial"}
+
+def make_donutplot(data, colors):
+    """Make donutplot w. labeled pieces"""
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.pie(data, wedgeprops = {'width':0.5}, startangle = 90, colors = colors)
+    if sum(data) > 100:
+        raise Exception("Data must not exceed 100 %.")
+    if len(data) != len(colors):
+        raise Exception("data and colors must be of same length!")
+
+def valuelabel_pie(sizes, labels, angles):
+    """Add valuelabels for circular pie chart"""
+    for i in range(len(sizes)):
+        angle = angles[i]
+        radian = angle * np.pi / 180
+        x = 0.8 * np.cos(radian)
+        y = 0.8 * np.sin(radian)
+        plt.text(x, y, labels[i], ha='center', va='center', size = 8, **text_font)
