554cc399fa4959e025a878528405d84210cf541a
Hallvard A. Wæhler <60000604+hallvaaw@users.noreply.github.com>
2023-11-07 14:53:54 +0100

    Create horizontal_grouped_barplot.py

---
+++ horizontal_grouped_barplot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+### primary colors
+## Orange, warm black
+primary_colors = ["#f26649", "#302026"]
+
+### secondary colors
+## Dk. grey, red, yellow, blue, aqua, lt. yellow
+secondary_colors = ["#525350", "#f15f5b", "#fce164", "#1c7ca1", "#71c3b4", "#fcf7ba"]
+
+title_font = {"fontname": "Century Gothic"}
+text_font = {"fontname": "Arial"}
+
+def make_grouped_barplot_h(barlists, barlabels, save_path, chart_title):
+   """Make horizontal grouped barplot"""
+   fig, ax = plt.subplots()
+   width = (1 / len(barlists) / 1.6)
+   for i, subbar in enumerate(barlists):
+       y_coords = [j + i*width - width*2 for j in range(len(subbar))]
+       ax.barh(y_coords, subbar, width, color = secondary_colors[i])
+   bary = list(range(len(subbar)))
+   plt.xticks(**text_font, size = 8)
+   plt.yticks(bary, barlabels, **text_font, size = 8, rotation = 0)
+   plt.box(False)
+   plt.tick_params(left=False)
+   plt.tick_params(bottom=False)
+   ax.set_axisbelow(True)
+   plt.grid(axis="x", linewidth = 0.2)
+   plt.savefig(f"{save_path}/{chart_title}.svg")
